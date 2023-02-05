fbf627eba96cc1cac1048636c2df673c56fca0a7
Hantao-Ye <hantaoye2000@gmail.com>
2023-02-05 00:24:55 -0800

    finish full-screen and csv upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 require("./planets")
 
+import '../static/style.css'
+
 function triggerToast() {
     const toastLiveExample = document.getElementById('liveToast')
     const toast = new bootstrap.Toast(toastLiveExample)
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [new HtmlWebpackPlugin()],
-    module: {
-        rules: [
-          {
-            test: /\.(txt|csv)$/,
-            use: [
-              {
-                loader: 'file-loader',
-              },
-            ],
-          },
-        ],
-      },
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	plugins: [
+		new HtmlWebpackPlugin(),
+		new CopyPlugin({
+			patterns: [
+				{ from: "./data", to: "." },
+			],
+		}),
+	],
+
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+		]
+	}
 };
